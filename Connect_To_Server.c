c0f8c2198f56c4ef901e1ba8063e221a6fec5f97
saivignesh-05 <saivikki301999@gmail.com>
2020-03-13 15:12:29 +0530

    fresh files of the deleted ones

---
+++ Connect_To_Server.c
@@ -0,0 +1,307 @@
+#include"Bingo_Header.h"
+extern int end_game_flag;
+
+void connect_to_server(char **err)
+{
+    *err=NULL;
+    end_game_flag=0;
+    int adl = sizeof(struct sockaddr),port_no;
+    
+    
+    struct sockaddr_in ad;
+    //int port;
+    game_p par;
+
+    
+
+    curs_set(1);
+    cbreak();
+    WINDOW *ser_det = newwin(20,50,6,50);
+	if(ser_det==NULL)
+	{
+	*err = "ser_det window error";
+	delwin(ser_det);
+	clear();
+	refresh();
+	return;
+
+	}
+
+    box(ser_det,0,0);
+
+
+    if(par.sersd = socket(AF_INET,SOCK_STREAM,0)==-1)		//otherwise check for errno
+	{
+
+	*err = "Socket create error";
+	delwin(ser_det);
+	clear();
+	refresh();
+	return;
+		
+	}
+		echo();
+
+    bzero(&ad,sizeof(ad));
+    ad.sin_family=AF_INET;
+    ad.sin_addr.s_addr=inet_addr(MY_ADDR);
+
+	while(1)
+	{
+
+    mvwprintw(ser_det,2,2,"USE ONLY BACKSPACE AND DIGITS!");
+    mvwprintw(ser_det,4,2,"ENTER PORT NUMBER(PRESS -1 TO EXIT):");
+
+    refresh();
+    wrefresh(ser_det);
+    wscanw(ser_det,"%d",&port_no);
+
+		if(port_no == -1)
+		{
+		    noecho();
+		    delwin(ser_det);
+		    clear();
+		    refresh();
+		    return;
+		}
+    ad.sin_port=htons(port_no);   
+	    if((connect(par.sersd,ADCAST&ad,adl))!=0)
+	    {
+		wattron(ser_det,COLOR_PAIR(4));
+	    mvwprintw(ser_det,15,2,"UNABLE TO CONNECT TO SERVER!!!");
+		wattroff(ser_det,COLOR_PAIR(4));
+		continue;
+	    }
+
+	    break;
+	}		//while close
+
+    noecho();
+
+    delwin(ser_det);
+    clear();
+    refresh();
+    
+    curs_set(0);
+    int startx = 7,starty = 60,row,col;
+
+    
+   for(int i=0;i<5;++i)
+    {
+	for(int j=0;j<5;++j)
+	{
+    	if(timed_recv(par.sersd,&par.get.array[i][j],sizeof(int),0,2)!=sizeof(int))
+	    {
+		close(par.sersd);
+		*err = "Array recieve error";
+		return;
+	    }
+
+	}
+
+    }
+ 
+    par.get.x = par.get.y = par.get.p =  par.get.q = 0;
+    pthread_mutex_init(&par.get.get_m,NULL);
+    pthread_mutex_init(&par.get.done_mutex,NULL);
+    pthread_cond_init(&par.get.done,NULL);
+
+    //BINGO IS FOR DISPLAYING THE ARRAY NUMBERS
+    //PLAYCHANCE FOR DISPLAYING WHO'S CHANCE IT IS
+    //BINGOCNT IS FOR DISPLAYING THE THE NUMBER OF ROWS,COLS AND DIAGONALS COMPLETED
+
+
+
+    par.playchance = newwin(3,120,3,starty);		//CREATES THE WINDOW AND RETURNS A POINTER TO THE PLAYCHANCE
+    if(par.playchance==NULL)				
+    {
+	*err="Unable to create WINDOW";				//ERROR MSG
+	return;
+    }
+
+    par.chancepan = new_panel(par.playchance);			//CREATES THE PANEL AND RETURN THE POINTER TO CHANCEPAN
+    if(par.chancepan==NULL)				
+    {
+	delwin(par.playchance);
+	//	pthread_mutex_destoy(&par.get.get_m);
+	//	pthread_mutex_destoy(&par.get.done_mutex);
+	//	pthread_cond_destoy(&par.get.done);
+	*err="Unable to create PANEL";				//ERROR MSG
+	return;
+    }
+
+    par.bingocnt = newwin(60,3,startx+2,starty-5);
+    if(par.bingocnt==NULL)				
+    {
+	del_panel(par.chancepan);
+	delwin(par.playchance);
+
+	*err="Unable to create WINDOW";				//ERROR MSG
+	return;
+    }
+
+    par.bingcnt = new_panel(par.bingocnt);
+    if(par.bingcnt==NULL)				
+    {
+	del_panel(par.chancepan);
+	delwin(par.playchance);
+
+	delwin(par.bingocnt);
+	*err="Unable to create PANEL";				//ERROR MSG
+	return;
+    }
+
+    ////////////////////////////////////////////////////		CREATING BINGO WINDOWS AND PANELS    
+
+    int i,j,t1,t2;
+
+    for(i=0;i<5;i++)
+    {
+	for(j=0;j<5;j++)
+	{
+	    par.get.bingo[i][j] = newwin(width,length,startx,starty);
+	    if(par.get.bingo[i][j]==NULL)
+	    {
+		del_panel(par.chancepan);
+		delwin(par.playchance);
+
+		del_panel(par.bingcnt);
+		delwin(par.bingocnt);
+
+		for(t1=0;t1<=i;++t1)
+		    for(t2=0;t2<5;++t2)
+		    {
+			if(t1==i&&t2==j)
+			    break;
+			delwin(par.get.bingo[t1][t2]);
+		    }
+		*err="Unable to create WINDOW";				//ERROR MSG
+		return;
+	    }
+
+	    par.pan[i][j] = new_panel(par.get.bingo[i][j]);
+	    if(par.pan[i][j]==NULL)
+	    {
+		del_panel(par.chancepan);
+		delwin(par.playchance);
+		del_panel(par.bingcnt);
+		delwin(par.bingocnt);
+		for(i=0;i<5;i++)
+		    for(j=0;j<5;j++)
+			delwin(par.get.bingo[i][j]);
+		for(t1=0;t1<=i;++t1)
+		    for(t2=0;t2<5;++t2)
+			{
+				if(t1==i&&t2==j)
+			 	   break;
+				del_panel(par.pan[t1][t2]);
+			}
+
+		*err="Unable to create PANEL";				//ERROR MSG
+		return;
+	    }
+
+	    wattron(par.get.bingo[i][j],COLOR_PAIR(1)|A_REVERSE);		//ENABLES ATTRIBUTES IN THE ARGUMENTS
+
+
+	    wborder(par.get.bingo[i][j],'|','|','-','-','+','+','+','+');	//FOR BORDERING THE WINDOW
+
+	    wattroff(par.get.bingo[i][j],COLOR_PAIR(1)|A_REVERSE);		//DISABLES ATTRIBUTES IN THE ARGUMENTS
+
+	    mvwprintw(par.get.bingo[i][j],2,3,"%d",par.get.array[i][j]);		//MOVES TO THE REQ POSITION AND PRINTS IN THE WINDOW
+
+	    starty+=right;
+
+	}
+	startx+=width-1;
+	starty = sy; 
+    }
+
+
+
+
+    wattron(par.get.bingo[0][0],A_STANDOUT);
+    mvwprintw(par.get.bingo[0][0],2,3,"%d",par.get.array[0][0]);
+    wattroff(par.get.bingo[0][0],A_STANDOUT);
+    update_panels();	
+    doupdate();				//REFRESHES ALL THE PANELS IN ORDER REQUIRED
+
+
+
+    if(pthread_create(&par.pid,NULL,get_key_t,&par.get)<0)
+    {
+	close(par.sersd);
+
+	for(t1=0;t1<5;++t1)
+	    for(t2=0;t2<5;++t2)
+	    {
+		delwin(par.get.bingo[t1][t2]);
+		del_panel(par.pan[t1][t2]);
+	    }
+	del_panel(par.chancepan);
+	delwin(par.playchance);
+	del_panel(par.bingcnt);
+	delwin(par.bingocnt);
+
+
+	*err="Unable to create Get Key thread";
+	//	pthread_mutex_destoy(&par.get.get_m);
+	//	pthread_mutex_destoy(&par.get.done_mutex);
+	//	pthread_cond_destoy(&par.get.done);
+	return;
+    }
+
+    if(pthread_create(&par.get.pid,NULL,client_game_t,&par)<0)
+    {
+	close(par.sersd);
+	pthread_cancel(par.pid);
+
+	for(t1=0;t1<5;++t1)
+	    for(t2=0;t2<5;++t2)
+	    {
+		del_panel(par.pan[t1][t2]);
+		delwin(par.get.bingo[t1][t2]);
+	    }
+	del_panel(par.chancepan);
+	delwin(par.playchance);
+	del_panel(par.bingcnt);
+	delwin(par.bingocnt);
+
+
+	*err="Unable to create Game thread";
+	//	pthread_mutex_destoy(&par.get.get_m);
+	//	pthread_mutex_destoy(&par.get.done_mutex);
+	//	pthread_cond_destoy(&par.get.done);
+	return;
+    }
+
+    while(1)
+    {
+	switch(end_game_flag)
+	{
+	    case 0:
+		sleep(0.2);
+		break;
+
+	    default:
+		close(par.sersd);
+		for(i=0;i<5;i++)
+		{
+		    for(j=0;j<5;j++)
+		    {
+			del_panel(par.pan[i][j]);
+			delwin(par.get.bingo[i][j]);
+		    }
+		}
+		del_panel(par.chancepan);
+		delwin(par.playchance);
+		del_panel(par.bingcnt);
+		delwin(par.bingocnt);
+
+		return;
+
+
+	}
+
+    }
+}
---
+++ Server_Game_Thread.c
@@ -0,0 +1,253 @@
+#include"Bingo_Header.h"
+int end_game_flag;
+void* serv_game_t(void* arg)
+{
+    char* err=NULL;
+    data d;
+    int i,j,status;
+    game_p *par=arg;
+    node *Current_player=par->get.pl.l.h,*tr;
+
+    int flag=0;
+    
+    while(Current_player!=Current_player->n)
+    {
+	if(Current_player==par->get.pl.l.h)
+	{
+	    wattron(par->playchance,COLOR_PAIR(2)|A_BOLD);
+	    mvwprintw(par->playchance,1,1,"       YOU ARE PLAYING     ");
+	    wattron(par->playchance,COLOR_PAIR(2)|A_BOLD); 
+	    update_panels();
+	    doupdate();
+	    pthread_mutex_unlock(&par->get.get_m);
+	    if(timedwait_cond(&par->get.done,&par->get.done_mutex,10)==0)
+	    {
+		pthread_mutex_lock(&par->get.get_m);
+		tr = Current_player;
+		d.num=((player*)Current_player->d)->array[par->get.p][par->get.q];
+		do
+		{
+		    search_strike(((player*)tr->d)->array,((player*)Current_player->d)->array[par->get.x][par->get.y],&i,&j);
+
+		    if((((player*)tr->d)->bngcnt+=bingos(((player*)tr->d)->array,i,j))>4)
+			flag=1;
+
+		    tr = tr->n;
+
+		}while(tr!=Current_player);
+		print_array(par->get.bingo,((player*)par->get.pl.l.h->d)->array,par->get.x,par->get.y);
+		bingodisp(par->bingont,((player*)par->get.pl.l.h->d)->bngcnt);
+
+	    }
+	    else		//OF TIMED WAIT
+	    {
+		pthread_mutex_lock(&par->get.get_m);
+		d.num=0;
+	    }
+	    if(flag!=1)
+	    {
+		tr=Current_player->n;
+		d.com='y';
+		d.opp=((player*)tr->d)->plid;
+		d.bng=((player*)tr->d)->bngcnt;
+		send(((player*)tr->d)->sd,&d,sizeof(data),0);
+		tr=tr->n;
+		d.com='n';
+		for(;tr!=Current_player->n;tr=tr->n)
+		    if(tr!=par->get.pl.l.h)
+		    {
+			d.bng=((player*)tr->d)->bngcnt;
+			send(((player*)tr->d)->sd,&d,sizeof(data),0);
+		    }
+	    }
+	    else
+	    {
+		tr=Current_player->n;
+		d.com='t';
+
+		for(;tr!=Current_player->n;tr=tr->n)
+		    if(tr!=par->get.pl.l.h)
+		    {
+			d.bng=((player*)tr->d)->bngcnt;
+			send(((player*)tr->d)->sd,&d,sizeof(data),0);
+		    }
+
+		if(((player*)par->get.pl.l.h->d)->bngcnt>4)
+		{
+		    wattron(par->playchance,COLOR_PAIR(2)|A_BOLD|A_BLINK);
+		    mvwprintw(par->playchance,1,1,"       YOU WON!!!!!        ");
+		    wattroff(par->playchance,COLOR_PAIR(2)|A_BOLD|A_BLINK); 
+		    update_panels();
+		    doupdate();
+		    sleep(2);
+		del_panel(par.chancepan);
+		delwin(par.playchance);
+		del_panel(par.bingcnt);
+		delwin(par.bingocnt);
+
+		for(t1=0;t1<5;++t1)
+		    for(t2=0;t2<5;++t2)
+		    {
+			del_panel(par.pan[t1][t2]);
+			delwin(par.get.bingo[t1][t2]);
+		    }
+
+		    pthread_cancel(par->pid);
+		    end_game_flag = 1;
+		    pthread_exit(NULL);
+		}
+		else
+		{
+		    wattron(par->playchance,COLOR_PAIR(2)|A_BOLD|A_BLINK);
+		    mvwprintw(par->playchance,1,1,"       YOU LOST           ");
+		    wattroff(par->playchance,COLOR_PAIR(2)|A_BOLD|A_BLINK); 
+		    update_panels();
+		    doupdate();		sleep(2);
+		del_panel(par.chancepan);
+		delwin(par.playchance);
+		del_panel(par.bingcnt);
+		delwin(par.bingocnt);
+
+		for(t1=0;t1<5;++t1)
+		    for(t2=0;t2<5;++t2)
+		    {
+			del_panel(par.pan[t1][t2]);
+			delwin(par.get.bingo[t1][t2]);
+		    }
+
+		    pthread_cancel(par->pid);
+		    end_game_flag = 2;
+		    pthread_exit(NULL);
+
+		}
+	    }
+	}
+	    wattron(par->playchance,COLOR_PAIR(2)|A_BOLD);
+	    mvwprintw(par->playchance,1,1,"        OPPONENT %d IS PLAYING     ",((player*)Current_player->d)->plid);
+	    wattron(par->playchance,COLOR_PAIR(2)|A_BOLD); 
+	    update_panels();
+	    doupdate();
+
+	if((status=timed_recv(((player*)Current_player)->sd,&d,sizeof(data),0,10))!=sizeof(data)&&status!=-1)
+	{
+		tr = Current_player->n;
+	    close(((player*)Current_player->d)->sd);
+		--(par->get.pl.n);
+		Current_player->n->p = Current_player->p;
+		Current_player->p->n = Current_player->n;
+		free(Current_player>d);
+		free(Current->player);
+
+Current_player = tr;
+continue;
+
+	}
+	else if(status==-1)
+	{
+	    d.num=0;
+	}
+	else
+	{	
+	    tr = Current_player;
+	    do
+	    {
+		search_strike(((player*)tr->d)->array,d.num,&i,&j);
+
+		if((((player*)tr->d)->bngcnt+=bingos(((player*)tr->d)->array,i,j))>4)
+		    flag=1;
+
+	    }while(tr!=Current_player);
+
+	print_array(par->get.bingo,((player*)par->get.pl.l.h->d)->array,par->get.x,par->get.y);
+	bingodisp(par->bingont,((player*)par->get.pl.l.h->d)->bngcnt);
+	}
+	if(flag!=1)
+	{
+	    tr=Current_player->n;
+	    d.com='y';
+	    d.opp=((player*)tr->d)->plid;
+	    d.bng=((player*)tr->d)->bngcnt;
+	    send(((player*)tr->d)->sd,&d,sizeof(data),0);
+	    tr=tr->n;
+	    d.com='n';
+	    for(;tr!=Current_player->n;tr=tr->n)
+		if(tr!=par->get.pl.l.h)
+		{
+		    d.bng=((player*)tr->d)->bngcnt;
+		    send(((player*)tr->d)->sd,&d,sizeof(data),0);
+		} 
+	}
+	else
+	{
+	    tr=Current_player->n;
+	    d.com='t';
+	    for(;tr!=Current_player->n;tr=tr->n)
+		if(tr!=par->get.pl.l.h)
+		{
+		    d.bng=((player*)tr->d)->bngcnt;
+		    send(((player*)tr->d)->sd,&d,sizeof(data),0);
+		}
+		if(((player*)par->get.pl.l.h->d)->bngcnt>4)
+		{
+		    wattron(par->playchance,COLOR_PAIR(2)|A_BOLD|A_BLINK);
+		    mvwprintw(par->playchance,1,1,"       YOU WON!!!!!        ");
+		    wattroff(par->playchance,COLOR_PAIR(2)|A_BOLD|A_BLINK); 
+		    update_panels();
+		    doupdate();
+		    sleep(2);
+		del_panel(par.chancepan);
+		delwin(par.playchance);
+		del_panel(par.bingcnt);
+		delwin(par.bingocnt);
+
+		for(t1=0;t1<5;++t1)
+		    for(t2=0;t2<5;++t2)
+		    {
+			del_panel(par.pan[t1][t2]);
+			delwin(par.get.bingo[t1][t2]);
+		    }
+
+		    pthread_cancel(par->pid);
+		    end_game_flag = 1;
+		    pthread_exit(NULL);
+
+		}
+
+		else
+		{
+		    wattron(par->playchance,COLOR_PAIR(2)|A_BOLD|A_BLINK);
+		    mvwprintw(par->playchance,1,1,"       YOU LOST           ");
+		    wattroff(par->playchance,COLOR_PAIR(2)|A_BOLD|A_BLINK); 
+		    update_panels();
+		    doupdate();		sleep(2);
+		del_panel(par.chancepan);
+		delwin(par.playchance);
+		del_panel(par.bingcnt);
+		delwin(par.bingocnt);
+
+		for(t1=0;t1<5;++t1)
+		    for(t2=0;t2<5;++t2)
+		    {
+			del_panel(par.pan[t1][t2]);
+			delwin(par.get.bingo[t1][t2]);
+		    }
+
+
+		    pthread_cancel(par->pid);
+		    end_game_flag = 2;
+		    pthread_exit(NULL);
+
+		}
+	}
+
+    }
+
+    end_game_flag=-1;
+    err="No other player playing";
+    pthread_cancel(par->pid);
+    close(((player*)trav->d)->sd);
+    trav=trav->n;
+    for(;trav!=par->get.pl.l.h;trav=trav->n)
+	close(((player*)trav->d)->sd);
+    pthread_exit(err);
+}

1557cce0a2e9fdbee4f510d9dc9f39ea53bbd131
arav12ind <saiaravind.sv@gmail.com>
2020-03-19 09:21:48 +0530

    Prints bingo array on screen

--- Print_Array.c
+++ Print_Array.c
@@ -1,4 +1,8 @@
 #include"Bingo_Header.h"
+/*
+ * Prints the array of numbers to the bingo window screen
+ * with x,y as the coordinates to highlight.
+ */
 void print_array(WINDOW *bingo[5][5],int array[5][5],int x,int y)
 {
     int i,j;
@@ -13,17 +17,18 @@ void print_array(WINDOW *bingo[5][5],int array[5][5],int x,int y)
 
 	    wattroff(bingo[i][j],COLOR_PAIR(1)|A_REVERSE);		//DISABLES ATTRIBUTES IN THE ARGUMENTS
 
-	    if(i!=x||j!=y)
+	    if(i!=x||j!=y)	// Checks whether to highlight the number or not.
 	    {
-		if(array[i][j]!=0)
+		if(array[i][j]!=0)	// If the number is non-zero print it as it is.
 		    mvwprintw(bingo[i][j],2,3,"%d",array[i][j]);
-		else
+		else		// else the number is 0 print 'x'
 		{
 		    wattron(bingo[i][j],COLOR_PAIR(3));
 		    mvwprintw(bingo[i][j],2,3,"X ");
 		    wattroff(bingo[i][j],COLOR_PAIR(3));
 		}
 	    }
+	    /***************** if the position is not highlighted  ******************/
 	    else
 	    {
 		if(array[i][j]!=0)

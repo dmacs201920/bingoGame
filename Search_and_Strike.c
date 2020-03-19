a8949b42f7de2b2e453aa4fedb60f22892b9bbf9
arav12ind <saiaravind.sv@gmail.com>
2020-03-19 10:14:44 +0530

    Searches for number in matrix ,gives it's position.

--- Search_and_Strike.c
+++ Search_and_Strike.c
@@ -1,4 +1,10 @@
 #include"Bingo_Header.h"
+/*
+ * This function takes the bingo array.( bingo )
+ * the number to search.( num )
+ * two integer pointers for output, the output is the position of the number. ( i , j )
+ * if num does not exist, function sets i and j as -1.
+ */
 void search_strike(int bingo[5][5],int num,int *i,int *j)
 {
     for((*i)=0;(*i)<5;++(*i))

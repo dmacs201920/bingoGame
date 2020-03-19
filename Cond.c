3b6596c66213e5b7fab0f4d4764735eae4d15220
saivignesh-05 <saivikki301999@gmail.com>
2020-03-19 15:50:58 +0530

    ADDED COMMENTS

--- Cond.c
+++ Cond.c
@@ -1,4 +1,11 @@
 #include"Bingo_Header.h"
+
+/*
+		FUNCTION FOR TIMED WAITING AND RECIEVING DATA FROM USER
+		IF THE TIME COMPLETES BEFORE THE USER ENTERS DATA, USER LOSES HIS CHANCE
+
+*/   
+
 void* cond_wait(void* arg)
 {
     cond_p *par=arg;
@@ -7,6 +14,7 @@ void* cond_wait(void* arg)
     pthread_cancel(par->tmr);
     pthread_exit(NULL);
 }
+/***************************************************** RUNS THE TIMER ************************************************************************/
 void* timer(void* arg)
 {
     cond_p *par=arg;
@@ -16,22 +24,32 @@ void* timer(void* arg)
     par->flag=2;
     pthread_exit(NULL);
 }
+/*********************************************************************************************************************************************/
+
+
 int timedwait_cond(pthread_cond_t *c,pthread_mutex_t *m,int sec)
 {
+    /**********************************************  INITIALIZATIONS  ***********************************************************************/
+
     cond_p par;
     par.c=c;
     par.m=m;
     par.sec=sec;
     par.flag=0;
-    if(pthread_create(&par.cwait,NULL,cond_wait,&par)!=0)
+
+    /***********************************************************************************************************************/
+
+    if(pthread_create(&par.cwait,NULL,cond_wait,&par)!=0) 		//CREATING THE RECIEVE FUNCTION 
     {
 	return -1;
     }
-    if(pthread_create(&par.tmr,NULL,timer,&par)!=0)
+    if(pthread_create(&par.tmr,NULL,timer,&par)!=0)			//CREATING THE TIMER FUNCTION
     {
 	pthread_cancel(par.cwait);
 	return -1;
     }
-    while(par.flag==0);
-    return par.flag-1;
+    while(par.flag==0);						// WAIT TILL FLAG CHANGED
+
+    return par.flag-1;						// IF FLAG = 2 TIME EXCEEDED WITHOUT INPUT
+    								// IF FLAG = 1 INPUT TAKEN WITHIN TIME
 }

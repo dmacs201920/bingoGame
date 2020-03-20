23b4f0f931c05ab0313bf31a696e63081db2c157
saivignesh-05 <saivikki301999@gmail.com>
2020-03-20 15:38:13 +0530

    About the project

--- About.c
+++ About.c
@@ -2,20 +2,25 @@
 void about()
 {
     attron(COLOR_PAIR(2));
-    mvwprintw(stdscr,1,65,"AUM SRI SAI RAM\n\n");
+    mvwprintw(stdscr,1,58,"AUM SRI SAI RAM\n\n");
     attroff(COLOR_PAIR(2));
+
     attron(COLOR_PAIR(6));
-    mvwprintw(stdscr,3,65,"ABOUT THE GAME\n\n");
+    mvwprintw(stdscr,3,58,"ABOUT THE GAME\n\n");
     attroff(COLOR_PAIR(6));
+
     attron(COLOR_PAIR(3));
-    printw("This program was created in the year 2020\n\n");
-    printw("By Sai Vignesh S. and Sai Aravind S.V.\n\n");
+    printw(" Works through inter process communication under the same computer\n\n");
+    printw(" This is a multi-threaded program using sockets and ncurses.\n\n\n\n");
     printw("Developed @ \n\tComputer Lab of Sri Sathya Sai Institute of Higher Learning, Brindavan Campus\n");
-    printw("Under the guidence of Darshan Gera, our computer science professor,");
-    printw("\nand all those people who helped us through this project");
+    printw("\n\n\n\nUnder the guidence of Darshan Gera and few alumni.\n\n\n\n");
+    printw(" By Sai Vignesh S. and Sai Aravind S.V.\n\n");
     attroff(COLOR_PAIR(3));
+
     attron(COLOR_PAIR(4));
     printw("\n\nPRESS q TO RETURN TO MAIN MENU\n");
     attroff(COLOR_PAIR(4));
+
+    refresh();
     while(getch()!='q');
 }
